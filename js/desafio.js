f9e8d360dca6651a3621aee9d8240a15720fcf3c
sirocogabriel <sirocogabriel@gmail.com>
2020-10-26 21:25:42 -0300

    botões (Proximo Anterior) Desafio

--- js/desafio.js
+++ js/desafio.js
@@ -1,3 +1,23 @@
-$(function(e) {
-  
-})
+function MudaDesafio(elem) {
+  var desafio = document.getElementById('desafio')
+
+  if (desafio) {
+    var value = desafio.getAttribute('value')
+
+    if (!value) {
+    }
+    console.log(value)
+  }
+
+  var img = new Image()
+  img.src = './assets/img/Capa.jpg'
+
+  img.onerror = function () {
+    console.log('Error bro')
+  }
+
+  img.onload = function () {
+    document.body.appendChild(img)
+    console.log('All right')
+  }
+}
